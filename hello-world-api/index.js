f2d3899feaa1b549d1358378e695bd1bba6352a9
EmanuelAugusto <emanuel02augusto@gmail.com>
2024-09-19 00:00:37 -0300

    stable

--- hello-world-api/index.js
+++ hello-world-api/index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const Redis = require("ioredis");
+const redisDb = new Redis();
+
 const port = 3000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json([]);
+app.get("/", async (req, res) => {
+  const KEY = "user";
+
+  const data = await redisDb.get(KEY);
+  res.json(JSON.parse(data));
 });
 
 app.listen(port, () => {
